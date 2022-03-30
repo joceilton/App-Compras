e95a5b21d653a01e3547a7a12f91108180699b2f
joceilton <joceilton.g@gmail.com>
2022-03-30 15:08:05 -0300

    Novas Alterações
    Adicionado o Tour e páginas estoque e carteira

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,36 @@ $(function() {
         menuPrincipal.toggle('slow')
     })
 
+    /* Tour */
+
+    //página Carteira
+    var carteira = new Anno([{
+        target  : '.saldo', // second block of code
+        position: 'top',
+        content : 'Aqui você vê o seu saldo.'
+      }, {
+        target  : '.compras',
+        position: 'top',
+        content : "Aqui você vê o seu saldo de compras.",
+      }, {
+        target  : '.extra',
+        position: 'top',
+        content : "Aqui você vê o seu saldo extra"
+      }, {
+        target  : '.adicionar-saldo-compras',
+        position: 'top',
+        content : "Retira de Saldo e adiciona para compras ou extra"
+      }, {
+        target  : '.adicionar-saldo-carteira',
+        position: 'bottom',
+        content : "Retira de Compras e adiciona para saldo ou extra"
+      }
+      ])
+      
+      carteira.show()
+
+
+
+
+
 })
